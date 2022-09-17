5e42aba856d3e7b6f590c2aa367b1d75795fb1ef
UNRIVALLEDKING <adityakumarunrivalledking@gmail.com>
2022-09-17 16:10:53 +0530

    Task Manager

---
+++ src/App.jsx
@@ -0,0 +1,125 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import laptop from "./assets/laptop.png";
+
+function App() {
+  // States
+
+  // const [allTasks, setAllTasks] = useState([]);
+  const [allTasks, setAllTasks] = useState(() => {
+    const savedTasks = localStorage.getItem("myTasks");
+    if (savedTasks) {
+      return JSON.parse(savedTasks);
+    } else {
+      return [];
+    }
+  });
+  const [task, setTask] = useState("");
+
+  // Functions
+
+  // Input Function
+  const handleInput = (e) => {
+    setTask(e.target.value);
+  };
+
+  // Add Task Function
+  const formSubmit = (e) => {
+    e.preventDefault();
+    if (task === "" || task === " ") {
+      setAllTasks([...allTasks]);
+    } else {
+      alert("Task Added");
+      setAllTasks([
+        ...allTasks,
+        { id: allTasks.length + 1, taskName: task.trim() },
+      ]);
+      setTask("");
+      console.log("all tasks", allTasks);
+    }
+  };
+
+  // Delete Task Function
+  const deleteTask = (id) => {
+    const removeTask = allTasks.filter((task) => {
+      console.log("remove", id, task.id, allTasks);
+      return task.id !== id;
+    });
+    setAllTasks(removeTask);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("myTasks", JSON.stringify(allTasks));
+  }, [allTasks]);
+
+  return (
+    <>
+      <div className="App">
+        <section className="container">
+          <div className="heading">
+            <img className="heading__img" src={laptop} />
+            <h1 className="heading__title">Task Manager</h1>
+          </div>
+          <form className="form" onSubmit={formSubmit}>
+            <div style={{ textAlign: "center" }}>
+              <label className="form__label" htmlFor="todo">
+                ~ My Tasks ~
+              </label>
+              <input
+                value={task}
+                onChange={handleInput}
+                placeholder="Add Food Items"
+                className="form__input"
+                type="text"
+                id="todo"
+                name="to-do"
+                size={30}
+              />
+              <button className="button" type="submit">
+                <span>Add Task</span>
+              </button>
+            </div>
+          </form>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-around",
+              alignItems: "baseline",
+            }}
+          >
+            <p style={{ marginBottom: "0px" }}>My Tasks:</p>
+            <button
+              className="button"
+              style={{ marginBottom: "0px", display: "inline", height: "26px" }}
+            >
+              <span style={{ padding: "0.5rem 0.4rem" }}>Veiw All Tasks</span>
+            </button>
+          </div>
+          <div
+            className="to_do_List"
+            style={{
+              marginTop: "1rem",
+              maxHeight: "380px",
+              overflowY: "scroll",
+              minHeight: "280px",
+              zIndex: "1",
+            }}
+          >
+            {allTasks.map((task, id) => (
+              <ul key={id} className="toDoList">
+                <li style={{ width: "75%", display: "inline-block" }}>
+                  <span>{id + 1} )</span> {task.taskName}{" "}
+                </li>
+                <button onClick={() => deleteTask(task.id)} className="button">
+                  <span> Delete Task </span>
+                </button>
+              </ul>
+            ))}
+          </div>
+        </section>
+      </div>
+    </>
+  );
+}
+
+export default App;
